fe8ce592657ca943ef3dcadb5bba7d48ed59b745
Dmitry <dimson-n@users.noreply.github.com>
2022-09-19 00:58:48 +0300

    Move dice roller parameters to separate file

--- Sources/DiceRoller.cs
+++ Sources/DiceRoller.cs
@@ -2,7 +2,7 @@
 
 namespace RP.ReverieWorld
 {
-    public sealed class DiceRoller
+    public sealed partial class DiceRoller
     {
         public const int DefaultDiceFacesCount = 6;
 
@@ -57,60 +57,6 @@ namespace RP.ReverieWorld
             this.randomProvider = randomProvider;
             this.defaultParameters = defaultParameters ?? new Parameters();
             ValidateParameters(this.defaultParameters);
-        }
-
-        public class Parameters : IParameters
-        {
-            public int FacesCount { get; init; }
-            public int DicesCount { get; init; }
-            public int AdditionalDicesCount { get; init; }
-            public int RerollsCount { get; init; }
-            public int BurstsCount { get; init; }
-            public int Bonus { get; init; }
-
-            public Parameters(int facesCount = DefaultDiceFacesCount, int dicesCount = 1, int additionalDicesCount = 0,
-                              int rerollsCount = 0, int burstsCount = 0, int bonus = 0)
-            {
-                FacesCount = facesCount;
-                DicesCount = dicesCount;
-                AdditionalDicesCount = additionalDicesCount;
-                RerollsCount = rerollsCount;
-                BurstsCount = burstsCount;
-                Bonus = bonus;
-            }
-        }
-
-        public static void ValidateParameters(IParameters? parameters)
-        {
-            if (parameters is null)
-            {
-                throw new ArgumentNullException(nameof(parameters));
-            }
-
-            if (parameters.FacesCount < 2)
-            {
-                throw new ArgumentOutOfRangeException(nameof(parameters.FacesCount), parameters.FacesCount, "Dice faces count can't be lesser than 2");
-            }
-
-            if (parameters.DicesCount < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(parameters.DicesCount), parameters.DicesCount, "Can't roll lesser than 1 dice");
-            }
-
-            if (parameters.AdditionalDicesCount < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(parameters.AdditionalDicesCount), parameters.AdditionalDicesCount, "Can't use negative count of additional dices");
-            }
-
-            if (parameters.RerollsCount < 0 && !parameters.HasInfinityRerolls)
-            {
-                throw new ArgumentException("Negative rerolls count available with infinity rerolls only", nameof(parameters.RerollsCount));
-            }
-
-            if (parameters.BurstsCount < 0 && !parameters.HasInfinityBursts)
-            {
-                throw new ArgumentException("Negative bursts count available with infinity bursts only", nameof(parameters.BurstsCount));
-            }
         }
 
         internal sealed class DiceData
