9eb0a48626d0175349d14c41a6e588013aa5f270
Dmitry <dimson-n@users.noreply.github.com>
2022-10-08 20:22:10 +0300

    Refactoring: move IDiceRemovingSelector to own file

--- Sources/AutoRoller.cs
+++ Sources/AutoRoller.cs
@@ -9,20 +9,6 @@ namespace RP.ReverieWorld.DiceRoll
         private readonly IDiceRemovingSelector diceRemovingSelector;
 
         /// <summary>
-        /// Strategy for "add then remove" dice mechanic.
-        /// </summary>
-        public interface IDiceRemovingSelector
-        {
-            /// <summary>
-            /// </summary>
-            /// <param name="dices">List of current dices values.</param>
-            /// <param name="count">Count of dices to remove.</param>
-            /// <param name="parameters">Parameters of current roll.</param>
-            /// <returns>Set of indices that must be removed with exactly <paramref name="count"/> elements.</returns>
-            IReadOnlySet<int> Select(IReadOnlyList<int> dices, int count, IParameters parameters);
-        }
-
-        /// <summary>
         /// </summary>
         /// <param name="randomProvider">Implementation of <see cref="IRandomProvider"/> interface.</param>
         /// <param name="defaultParameters">Custom implementation of <see cref="IParameters"/> interface or <see cref="Parameters"/> (default).</param>
---
+++ Sources/IDiceRemovingSelector.cs
@@ -0,0 +1,16 @@
+﻿namespace RP.ReverieWorld.DiceRoll
+{
+    /// <summary>
+    /// Strategy for "add then remove" dice mechanic for <see cref="AutoRoller"/>.
+    /// </summary>
+    public interface IDiceRemovingSelector
+    {
+        /// <summary>
+        /// </summary>
+        /// <param name="dices">List of current dices values.</param>
+        /// <param name="count">Count of dices to remove.</param>
+        /// <param name="parameters">Parameters of current roll.</param>
+        /// <returns>Set of indices that must be removed with exactly <paramref name="count"/> elements.</returns>
+        IReadOnlySet<int> Select(IReadOnlyList<int> dices, int count, IParameters parameters);
+    }
+}
