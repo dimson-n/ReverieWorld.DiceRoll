9de2db88c06a91af59f95c829a6cfac5ee220802
Dmitry <dimson-n@users.noreply.github.com>
2022-10-09 13:27:39 +0300

    Move `IParameters` interface to separate file

--- Sources/Parameters.cs
+++ Sources/Parameters.cs
@@ -1,18 +1,5 @@
 ﻿namespace RP.ReverieWorld.DiceRoll
 {
-    public interface IParameters
-    {
-        int FacesCount { get; }
-        int DicesCount { get; }
-        int AdditionalDicesCount { get; }
-        int RerollsCount { get; }
-        int BurstsCount { get; }
-        int Bonus { get; }
-
-        bool HasInfinityRerolls => RerollsCount < 0;
-        bool HasInfinityBursts => BurstsCount < 0;
-    }
-
     /// <summary>
     /// Default implementation of <see cref="IParameters"/> interface.
     /// </summary>
