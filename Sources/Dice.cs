1e587a9e5eb86796579a261ffce87fb4d9f11ad2
Dmitry <dimson-n@users.noreply.github.com>
2022-10-13 21:17:55 +0300

    More docstrings

--- Sources/Dice.cs
+++ Sources/Dice.cs
@@ -3,7 +3,7 @@
 namespace RP.ReverieWorld.DiceRoll
 {
     /// <summary>
-    /// Represents one dice in roll.
+    /// Represents one dice in the <see cref="Roll"/>.
     /// </summary>
     public sealed class Dice : IReadOnlyList<int>
     {
@@ -11,15 +11,27 @@ namespace RP.ReverieWorld.DiceRoll
         internal bool burstMade;
 
         /// <summary>
-        /// Actual value of dice.
+        /// Gets actual value of the dice.
         /// </summary>
+        /// <returns>Actual value of the dice.</returns>
         public int Value => values.Last();
 
+        /// <summary>
+        /// Gets the number of rolls of the <see cref="Dice"/>.
+        /// </summary>
+        /// <returns>The number of rolls of the <see cref="Dice"/>.</returns>
         public int RollsCount => values.Count;
 
-        /// <returns><see langword="true"/> if dice was excluded from roll result; otherwise, <see langword="false"/>.</returns>
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="Dice"/> excluded from roll result.
+        /// </summary>
+        /// <returns><see langword="true"/> if the <see cref="Dice"/> was excluded from roll result; otherwise, <see langword="false"/>.</returns>
         public bool Removed { get; internal set; }
 
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="Dice"/> made as burst.
+        /// </summary>
+        /// <returns><see langword="true"/> if the <see cref="Dice"/> was made as burst; otherwise, <see langword="false"/>.</returns>
         public bool IsBurst { get; internal set; }
 
         internal Dice(int value, bool removed = false, bool burstMade = false, bool isBurst = false)
@@ -30,14 +42,28 @@ namespace RP.ReverieWorld.DiceRoll
             this.IsBurst = isBurst;
         }
 
+        /// <summary>
+        /// Gets the dice roll value at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the dice roll to get.</param>
+        /// <returns>The dice roll value at the specified index.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public int this[int index] => values[index];
 
         int IReadOnlyCollection<int>.Count => values.Count;
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection of dice rolls.
+        /// </summary>
+        /// <returns>An enumerator for <see cref="IReadOnlyList{Int32}"/>.</returns>
         public IEnumerator<int> GetEnumerator() => values.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)values).GetEnumerator();
 
+        /// <summary>
+        /// Returns a string that represents the <see cref="Dice"/>.
+        /// </summary>
+        /// <returns>A string that represents the <see cref="Dice"/>.</returns>
         public override string ToString()
         {
             return $"{(Removed ? '-' : string.Empty)}{(IsBurst ? '*' : string.Empty)}{Value}";
