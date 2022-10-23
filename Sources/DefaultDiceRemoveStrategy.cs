2659bd14736aaf4b2963f7d04405354e5b6e6784
Dmitry <dimson-n@users.noreply.github.com>
2022-10-24 01:39:59 +0300

    Refactoring: formatting

--- Sources/DefaultDiceRemoveStrategy.cs
+++ Sources/DefaultDiceRemoveStrategy.cs
@@ -13,9 +13,13 @@ namespace RP.ReverieWorld.DiceRoll
             // TODO: can try boundary+=1 if there is much more rerolls then ones as if parameters.HasInfinityRerolls.
             int nonRerollBoundary = parameters.FacesCount / 2 + (parameters.HasInfinityRerolls ? 1 : 0);
 
-            var sortedRolls = dices.Select((dice, idx) => (dice.Value, idx)).OrderBy(d => d.Value);
-            int mayBeRemoved = Math.Max(count, sortedRolls.TakeWhile(d => d.Value < nonRerollBoundary).Count());
-            int rerollableCount = sortedRolls.TakeWhile(d => d.Value == 1).Count();
+            var sortedRolls = dices.Select((dice, idx) => (dice.Value, idx))
+                                   .OrderBy(d => d.Value);
+            int mayBeRemoved = Math.Max(count,
+                                        sortedRolls.TakeWhile(d => d.Value < nonRerollBoundary)
+                                                   .Count());
+            int rerollableCount = sortedRolls.TakeWhile(d => d.Value == 1)
+                                             .Count();
             int skipToReroll = Math.Min(mayBeRemoved - count,
                                         Math.Min(rerollableCount,
                                                  parameters.HasInfinityRerolls ? int.MaxValue : parameters.RerollsCount));
