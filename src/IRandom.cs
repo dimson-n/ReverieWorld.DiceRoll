f6773ade74822eb65d19e2c058e78332ad8498e7
Dmitry <dimson-n@users.noreply.github.com>
2022-10-30 02:48:26 +0300

    More docstrings

--- src/IRandom.cs
+++ src/IRandom.cs
@@ -1,16 +1,27 @@
 ﻿namespace RP.ReverieWorld.DiceRoll;
 
+/// <summary>
+/// Provides an abstraction for (pseudo)random number generator for dice rollers.
+/// </summary>
 public interface IRandom : IDisposable
 {
     /// <summary>
-    /// Same behavior as <see cref="System.Random.Next(int)"/> expected.
+    /// Returns a non-negative random integer that is less than the specified maximum.
     /// </summary>
+    /// <remarks>Same behavior as <see cref="System.Random.Next(int)"/> expected.</remarks>
     /// <param name="maxValue">The exclusive upper bound of the random number to be generated. <paramref name="maxValue"/> must be greater than or equal to 0.</param>
-    /// <returns></returns>
+    /// <returns>A 32-bit signed integer that is greater than or equal to 0, and less than <paramref name="maxValue"/>.</returns>
     int Next(int maxValue);
 }
 
+/// <summary>
+/// Provides an abstraction for (pseudo)random number generator provider for dice rollers.
+/// </summary>
 public interface IRandomProvider
 {
+    /// <summary>
+    /// Gets an instance of (pseudo)random number generator for dice rollers.
+    /// </summary>
+    /// <returns>An instance of (pseudo)random number generator for dice rollers.</returns>
     IRandom Lock();
 }
