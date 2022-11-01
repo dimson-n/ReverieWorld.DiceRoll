53ace59531e13cddfb7078a202b37a2379ba673d
Dmitry <dimson-n@users.noreply.github.com>
2022-11-01 23:49:11 +0300

    Implement default `IRandomProvider`

---
+++ src/DefaultRandomProvider.cs
@@ -0,0 +1,23 @@
+﻿namespace RP.ReverieWorld.DiceRoll;
+
+/// <summary>
+/// Default thread-safe implementation of PRNG for dice rollers.
+/// </summary>
+/// <remarks>Recomended to implement it in you own way.</remarks>
+public sealed class DefaultRandomProvider : IRandomProvider
+{
+    /// <summary>
+    /// Gets a thread-safe instance of PRNG for dice rollers.
+    /// </summary>
+    /// <returns>A thread-safe instance of PRNG for dice rollers.</returns>
+    public IRandom Lock() => new RandomImpl();
+
+    private sealed class RandomImpl : IRandom
+    {
+        private readonly Random rnd = Random.Shared;
+
+        public int Next(int maxValue) => rnd.Next(maxValue);
+
+        public void Dispose() { }
+    }
+}
