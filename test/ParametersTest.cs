2e99dd9102339e1b40a2f6b32b009ddeacc1465e
Dmitry <dimson-n@users.noreply.github.com>
2022-11-03 00:45:58 +0300

    Add parameter fields tests

--- test/ParametersTest.cs
+++ test/ParametersTest.cs
@@ -1,9 +1,34 @@
 namespace RP.ReverieWorld.DiceRoll.Tests;
 
-public class ParametersTest
+public sealed class ParametersTest
 {
     [Fact]
-    public void TestValidation()
+    public void Defaults()
+    {
+        Parameters p = new();
+
+        Assert.Equal(6, p.FacesCount);
+        Assert.Equal(1, p.DicesCount);
+        Assert.Equal(0, p.AdditionalDicesCount);
+        Assert.Equal(0, p.RerollsCount);
+        Assert.Equal(0, p.BurstsCount);
+        Assert.Equal(0, p.Bonus);
+
+        Assert.False(p.HasInfinityRerolls);
+        Assert.False(p.HasInfinityBursts);
+    }
+
+    [Fact]
+    public void InfinityFields()
+    {
+        Parameters p = new(rerollsCount: Parameters.Infinite, burstsCount: Parameters.Infinite);
+
+        Assert.True(p.HasInfinityRerolls);
+        Assert.True(p.HasInfinityBursts);
+    }
+
+    [Fact]
+    public void Validation()
     {
         Assert.Throws<ArgumentNullException>("parameters", () => Parameters.Validate(null!));
 
