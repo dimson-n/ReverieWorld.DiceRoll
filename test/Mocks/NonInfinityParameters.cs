0ac28bd66969a2e587511bdf705fa3529ce34d27
Dmitry <dimson-n@users.noreply.github.com>
2023-02-07 01:38:22 +0300

    Simplify mock class by usage

--- test/Mocks/NonInfinityParameters.cs
+++ test/Mocks/NonInfinityParameters.cs
@@ -5,9 +5,8 @@ internal sealed class NonInfinityParameters : Parameters
     public override bool HasInfinityRerolls => false;
     public override bool HasInfinityBursts => false;
 
-    public NonInfinityParameters(int dicesCount = 1, int additionalDicesCount = 0,
-                                 int rerollsCount = 0, int burstsCount = 0, int bonus = 0) :
-        base(dicesCount, additionalDicesCount, rerollsCount, burstsCount, bonus)
+    public NonInfinityParameters(int rerollsCount = 0, int burstsCount = 0) :
+        base(rerollsCount: rerollsCount, burstsCount: burstsCount)
     {
     }
 }
