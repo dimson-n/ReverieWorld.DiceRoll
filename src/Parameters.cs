3d076521d9b31759628fa0aeccd58c3c299a8d19
Dmitry <dimson-n@users.noreply.github.com>
2023-05-18 01:37:12 +0300

    Simplify dice faces count constant name

--- src/Parameters.cs
+++ src/Parameters.cs
@@ -10,9 +10,9 @@ public class Parameters : GenericParameters
     internal static readonly Parameters Default = new();
 
     /// <summary>
-    /// Default value for <see cref="GenericParameters.FacesCount"/> property unless otherwise specified.
+    /// Predefined value for <see cref="GenericParameters.FacesCount"/> property.
     /// </summary>
-    public const int DefaultDiceFacesCount = 6;
+    public const int DiceFacesCount = 6;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="Parameters"/> class with designated parameters for a dice roller.
@@ -25,7 +25,7 @@ public class Parameters : GenericParameters
     /// <param name="modifiers">Optional modifiers for a roll.</param>
     public Parameters(int dicesCount = 1, int additionalDicesCount = 0, int rerollsCount = 0,
                       int burstsCount = 0, int bonus = 0, IReadOnlyCollection<IRollModifier>? modifiers = null) :
-        base(DefaultDiceFacesCount, dicesCount, additionalDicesCount, rerollsCount, burstsCount, bonus, modifiers)
+        base(DiceFacesCount, dicesCount, additionalDicesCount, rerollsCount, burstsCount, bonus, modifiers)
     {
     }
 
