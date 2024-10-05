826d10b116eb2aba6a7f3e405735f631ef328acf
Dmitry <dimson-n@users.noreply.github.com>
2024-10-05 16:54:04 +0300

    Add debug view for parameter counter

--- src/Utils/ParamCounter.cs
+++ src/Utils/ParamCounter.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics;
+
 namespace ReverieWorld.DiceRoll.Utils;
 
+[DebuggerDisplay("{infinite ? \"∞\" : count.ToString()}")]
 internal readonly struct ParamCounter
 {
     private readonly int count;
